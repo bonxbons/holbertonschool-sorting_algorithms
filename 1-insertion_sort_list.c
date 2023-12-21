be0041c0a3c2578189ffd7f6016e5716912ceed0
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 16:44:57 -0600

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,38 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list using insertion sort.
+ * @list: A pointer to the head of the list.
+ */
 void insertion_sort_list(listint_t **list)
 {
-    if (*list == NULL || (*list)->next == NULL) {
-        return;  /* List is empty or has only one element */
-    }
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-    /* Declare variables at the beginning of the block */
-    listint_t *current, *next;
+    listint_t *current, *temp;
 
-    for (current = (*list)->next; current != NULL; current = next) {
-        next = current->next;
+    for (current = (*list)->next; current != NULL; current = temp)
+    {
+        temp = current->next;
 
-        /* Find insertion point */
-        listint_t *prev = NULL;
-        listint_t *temp = *list;
-        while (temp != NULL && temp->n < current->n) {
-            prev = temp;
-            temp = temp->next;
+        while (current->prev != NULL && current->n < current->prev->n)
+        {
+            /* Swap nodes in the list */
+            current->prev->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+            
+            current->next = current->prev;
+            current->prev = current->next->prev;
+
+            if (current->prev != NULL)
+                current->prev->next = current;
+            else
+                *list = current;
+
+            current->next->prev = current;
+            
+            print_list(*list);
         }
-
-        /* Detach current node */
-        if (prev != NULL) {
-            prev->next = current->next;
-            if (current->next != NULL) {
-                current->next->prev = prev;
-            }
-        } else {
-            /* Detaching head node */
-            *list = current->next;
-            if (current->next != NULL) {
-                current->next->prev = NULL;
-            }
-        }
-
-        /* Insert current node before temp */
-        current->next = temp;
-        current->prev = prev;
-        if (temp != NULL) {
-            temp->prev = current;
-        } else {
-            /* Inserted at the end */
-            *list = current;
-        }
-
-        /* Print the list after the insertion */
-        print_list(*list);
     }
 }
