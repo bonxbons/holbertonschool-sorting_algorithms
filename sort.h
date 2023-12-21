6c9bbdcca4821a226e34dcd147e4a637e444c0ac
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 16:23:17 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -16,7 +16,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
 /* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
