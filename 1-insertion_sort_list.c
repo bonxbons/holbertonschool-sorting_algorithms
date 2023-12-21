91468828be668ac4feff8a949933e336c05e5493
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 16:42:17 -0600

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,49 +3,48 @@
 void insertion_sort_list(listint_t **list)
 {
     if (*list == NULL || (*list)->next == NULL) {
-        return;  // List is empty or has only one element
+        return;  /* List is empty or has only one element */
     }
 
-    // Move variable declarations to the beginning of the block
+    /* Declare variables at the beginning of the block */
     listint_t *current, *next;
-    listint_t *prev, *temp;
 
     for (current = (*list)->next; current != NULL; current = next) {
         next = current->next;
 
-        // Find insertion point
-        prev = NULL;
-        temp = *list;
+        /* Find insertion point */
+        listint_t *prev = NULL;
+        listint_t *temp = *list;
         while (temp != NULL && temp->n < current->n) {
             prev = temp;
             temp = temp->next;
         }
 
-        // Detach current node
+        /* Detach current node */
         if (prev != NULL) {
             prev->next = current->next;
             if (current->next != NULL) {
                 current->next->prev = prev;
             }
         } else {
-            // Detaching head node
+            /* Detaching head node */
             *list = current->next;
             if (current->next != NULL) {
                 current->next->prev = NULL;
             }
         }
 
-        // Insert current node before temp
+        /* Insert current node before temp */
         current->next = temp;
         current->prev = prev;
         if (temp != NULL) {
             temp->prev = current;
         } else {
-            // Inserted at the end
+            /* Inserted at the end */
             *list = current;
         }
 
-        // Print the list after the insertion
+        /* Print the list after the insertion */
         print_list(*list);
     }
 }
