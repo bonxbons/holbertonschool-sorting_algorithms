c47928055219443b93d19af710583d4f9f94f29f
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 16:18:54 -0600

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,4 @@
-#include <stdio.h>
-
-/**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i != size - 1)
-            printf(", ");
-    }
-    printf("\n");
-}
+#include "sort.h"
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
