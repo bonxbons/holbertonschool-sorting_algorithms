6a8039c49f2d8415f9da129ec0215370a440777c
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 20:08:57 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,39 +1,50 @@
 #include "sort.h"
 
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ *              using the Quick sort algorithm (Lomuto partition scheme).
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
 void quick_sort(int *array, size_t size)
 {
-    size_t pivot_index;  /* Declare pivot_index at the beginning*/
+	if (size <= 1)
+		return; /* Base case: array with 0 or 1 element is already sorted */
 
-    if (size <= 1) {
-        return;  /* Base case: array with 0 or 1 element is already sorted */
-    }
-
-    pivot_index = partition(array, size);
-    quick_sort(array, pivot_index);
-    quick_sort(array + pivot_index + 1, size - pivot_index - 1);
+	size_t pivot = partition(array, size);
+	quick_sort(array, pivot);
+	quick_sort(array + pivot + 1, size - pivot - 1);
 }
 
-size_t partition(int *array, size_t size){
-    int pivot = array[size - 1];
-    int temp;
-    size_t i = 0, j;
+/**
+ * partition - Partitions an array using the Lomuto partition scheme
+ * @array: The array to be partitioned
+ * @size: The size of the array
+ * Return: The index of the pivot element after partitioning
+ */
+size_t partition(int *array, size_t size)
+{
+	int pivot = array[size - 1];
+	int temp;
+	size_t i = 0, j;
 
-    for (j = 0; j < size - 1; j++) {
-        if (array[j] <= pivot) {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-        }
-    }
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+		}
+	}
 
-    /* Swap pivot with element at i*/
-    temp = array[i];
-    array[i] = array[size - 1];
-    array[size - 1] = temp;
+	/* Swap pivot with element at i */
+	temp = array[i];
+	array[i] = array[size - 1];
+	array[size - 1] = temp;
 
-    print_array(array, size);
+	print_array(array, size);
 
-    return i;
+	return i;
 }
-
