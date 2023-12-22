9523a23f770bee66bdfc276ed9ed3421bfec4b1b
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 19:44:16 -0600

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,7 +2,7 @@
 
 void quick_sort(int *array, size_t size)
 {
-    size_t pivot_index;  // Declare pivot_index at the beginning
+    size_t pivot_index;  /* Declare pivot_index at the beginning*/
 
     if (size <= 1) {
         return;  /* Base case: array with 0 or 1 element is already sorted */
@@ -26,7 +26,7 @@ size_t partition(int *array, size_t size){
         }
     }
 
-    // Swap pivot with element at i
+    /* Swap pivot with element at i*/
     int temp = array[i];
     array[i] = array[size - 1];
     array[size - 1] = temp;
