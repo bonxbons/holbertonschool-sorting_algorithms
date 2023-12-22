4abac2a8e7f6cd7e1c066e5b27d74c13d8d04989
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 20:28:39 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);  /*Include return type*/
 int partition(int *array, int low, int high, size_t size); /* Include return type*/
+void sort(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
